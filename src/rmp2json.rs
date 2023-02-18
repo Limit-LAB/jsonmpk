08df6aefb280c0568baebeb6b4b4ee2fff0d7a40
imlyzh <enterindex@gmail.com>
2023-02-18 23:07:45 +0800

    fixed uint read bug

--- src/rmp2json.rs
+++ src/rmp2json.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use rmp::decode::{*, bytes::Bytes};
 use rmp::Marker::*;
 use serde_json::{Value, Number, Map};
@@ -33,25 +35,29 @@ fn utils_read_str(rd: &mut Bytes) -> Result<String, ()> {
   Err(())
 }
 
+fn err_handle<T: Debug>(e: T) -> () {
+  println!("error: {:?}", e);
+}
+
 #[allow(clippy::result_unit_err)]
 pub fn read_value(rd: &mut Bytes) -> Result<Value, ()> {
-  let m = read_marker(rd).map_err(|_e| ())?;
+  let m = read_marker(rd).map_err(err_handle)?;
   let r = match m {
     FixPos(val) => Value::Number(Number::from(val)),
     FixNeg(val) => Value::Number(Number::from(val)),
     Null  => Value::Null,
     True  => Value::Bool(true),
     False => Value::Bool(false),
-    U8  => Value::Number(Number::from(read_u8 (rd).map_err(|_e| ())?)),
-    U16 => Value::Number(Number::from(read_u16(rd).map_err(|_e| ())?)),
-    U32 => Value::Number(Number::from(read_u32(rd).map_err(|_e| ())?)),
-    U64 => Value::Number(Number::from(read_u64(rd).map_err(|_e| ())?)),
-    I8  => Value::Number(Number::from(read_i8 (rd).map_err(|_e| ())?)),
-    I16 => Value::Number(Number::from(read_i16(rd).map_err(|_e| ())?)),
-    I32 => Value::Number(Number::from(read_i32(rd).map_err(|_e| ())?)),
-    I64 => Value::Number(Number::from(read_i64(rd).map_err(|_e| ())?)),
-    F32 => Value::Number(Number::from_f64(read_f32(rd).map_err(|_e| ())?.into()).unwrap()),
-    F64 => Value::Number(Number::from_f64(read_f64(rd).map_err(|_e| ())?).unwrap()),
+    U8  => Value::Number(Number::from(rd.read_data_u8().map_err(err_handle)?)),
+    U16 => Value::Number(Number::from(rd.read_data_u16().map_err(err_handle)?)),
+    U32 => Value::Number(Number::from(rd.read_data_u32().map_err(err_handle)?)),
+    U64 => Value::Number(Number::from(rd.read_data_u64().map_err(err_handle)?)),
+    I8  => Value::Number(Number::from(rd.read_data_i8().map_err(err_handle)?)),
+    I16 => Value::Number(Number::from(rd.read_data_i16().map_err(err_handle)?)),
+    I32 => Value::Number(Number::from(rd.read_data_i32().map_err(err_handle)?)),
+    I64 => Value::Number(Number::from(rd.read_data_i64().map_err(err_handle)?)),
+    F32 => Value::Number(Number::from_f64(rd.read_data_f32().map_err(err_handle)?.into()).unwrap()),
+    F64 => Value::Number(Number::from_f64(rd.read_data_f64().map_err(err_handle)?).unwrap()),
     FixStr(size) => {
       let size = size as usize;
       let str = (0..size).map(|_| rd.read_u8().unwrap()).collect::<Vec<u8>>();
@@ -75,19 +81,19 @@ pub fn read_value(rd: &mut Bytes) -> Result<Value, ()> {
     FixArray(size) => {
       let size = size as usize;
       let arr = (0..size).map(|_| read_value(rd)).collect::<Result<Vec<_>, _>>();
-      let arr = arr.map_err(|_e| ())?;
+      let arr = arr.map_err(err_handle)?;
       Value::Array(arr)
     },
     Array16 => {
       let size = rd.read_data_u16().unwrap() as usize;
       let arr = (0..size).map(|_| read_value(rd)).collect::<Result<Vec<_>, _>>();
-      let arr = arr.map_err(|_e| ())?;
+      let arr = arr.map_err(err_handle)?;
       Value::Array(arr)
     },
     Array32 => {
       let size = rd.read_data_u32().unwrap() as usize;
       let arr = (0..size).map(|_| read_value(rd)).collect::<Result<Vec<_>, _>>();
-      let arr = arr.map_err(|_e| ())?;
+      let arr = arr.map_err(err_handle)?;
       Value::Array(arr)
     },
     FixMap(size) => {
--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,5 @@
 
+use rmp::Marker;
 use serde_derive::{Serialize, Deserialize};
 
 use jsonmpk::*;
@@ -11,18 +12,35 @@ pub struct Foo {
   age: u8,
   num: i32,
   name: String,
+  access: bool,
 }
 
 
 #[test]
 fn test() {
-  rmp2json_once(&Foo::default());
-  json2rmp_once(&Foo::default());
+  let test_cases = [
+    Foo::default(),
+    Foo {
+        age: 11,
+        num: 114514,
+        name: "1145141919810".to_string(),
+        access: true,
+    }
+  ];
+  for i in test_cases {
+    rmp2json_once(&i);
+    json2rmp_once(&i);
+  }
 }
 
 fn rmp2json_once(foo: &Foo) {
   let foo = rmp_serde::to_vec_named(foo).unwrap();
-
+  for i in foo.iter() {
+    print!("{:#08b}\t", i);
+    print!("{:?}", Marker::from_u8(*i));
+    println!("");
+  }
+  println!("------------------");
 
   let foo1 = rmp_serde::from_slice::<Foo>(&foo).unwrap();
   let foo2 = serde_json::from_value::<Foo>(Value::from_rmp(&foo).unwrap()).unwrap();
